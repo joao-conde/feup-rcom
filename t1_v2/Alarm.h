ef1d31b48deef638f74901b602306fc14777f624
Quaresma1997 <Quaresma1997@users.noreply.github.com>
2017-10-22 10:11:27 +0100

    Add files via upload

---
+++ t1_v2/Alarm.h
@@ -0,0 +1,7 @@
+extern int flag; //extern allows the variable to be instantiated in another file
+
+void atende(int signal);
+
+void setAlarm();
+
+void stopAlarm();
---
+++ t1_v2/AppLayer.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <termios.h>
+
+int sendFile(const char* path, int file_fd, char *file_name);
+int receiveFile(const char* path);
+
+int sendControlPacket(int fd, int file_size, char* file_name, int control_field);
+int receiveControlPacket(unsigned char * buffer, int control_field, unsigned int * file_size, char ** file_name);
+
+int sendDataPackage(int fd, int N, const char* buf, int length);
+int receiveDataPackage(int fd, int* N, char** buf, int* length);

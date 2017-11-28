945c68bc6494612e53c976f72567907277dd5955
JC13 <joaoconde_13@hotmail.com>
2017-11-28 09:30:39 +0000

    main basic logic operations

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -1,25 +1,83 @@
-#include "url.h"
+#include "URL.h"
+#include "FTP.h"
 
 
 int main(int argc, char** argv){
 
-  if(argc != 2){
-    printf("WRONG NUMBER OF ARGUMENTS\n");
-    printUsage(argv[0]);
-  }
+/*
 
-  	///////////// URL PROCESS /////////////
-    url url;
-    initURL(&url);
+ In order to connect to FTP service and download file we must:
+	1-proccess the URL (given as argument): retrieve useful information from the URL and find out the host IP
+	2-process FTP client:
+		-connect to FTP
+		-login in FTP (or anonymous)
+		-change directory
+		-set passive mode
+		-begin transmitting
+		-file download
+		-disconnect from FTP
+*/
 
 
-    // start parsing argv[1] to URL components
-    if (parseURL(&url, argv[1]))
-      return -1;
+	if(argc != 2){
+		printf("wrong number of arguments\n");
+		return 1;
+	}
+
+
+	//url process
+	url url;
+	initURL(&url);
+	parseURL(&url,argv[1]);
+
+	//get hostname IP
+	getIpByHost(&url);
+
+
+	//process FTP client
+	ftp ftp;
+
+	//connect
+	ftpConnect(&ftp, url.ip, url.port);
+
+	//user and pw
+	const char* user = strlen(url.user) ? url.user : "anonymous";
+	char* password;
+	if (strlen(url.password)) {
+		password = url.password;
+	} else {
+		char buf[100];
+		printf("You are now entering in anonymous mode.\n");
+		printf("Please insert your college email as password: ");
+		while (strlen(fgets(buf, 100, stdin)) < 14)
+			printf("\nIncorrect input, please try again: ");
+		password = (char*) malloc(strlen(buf));
+		strncat(password, buf, strlen(buf) - 1);
+	}
+
+
+	//login
+	ftpLogin(&ftp,user,password);
+
+	//change directory
+	ftpCWD(&ftp,url.path);
+
+	//enter passive mode
+	ftpPasv(&ftp);
+
+	//begin transmitting
+	ftpRetr(&ftp,url.filename);
+
+	//download filename
+	ftpDownload(&ftp,url.filename);
+
+	//disconnect from FTP
+	ftpDisconnect(&ftp);
+
+	return 0;
 }
 
-
 void printUsage(char* argv0) {
-	printf("\nUsage1 Normal: %s ftp://[<user>:<password>@]<host>/<url-path>\n",	argv0);
-	printf("Usage2 Anonymous: %s ftp://<host>/<url-path>\n\n", argv0);
+	printf("\nLogin Usage: %s ftp://[<user>:<password>@]<host>/<url-path>\n",	argv0);
+	printf("Anonymous Usage: %s ftp://<host>/<url-path>\n\n", argv0);
 }
